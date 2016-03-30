9a79a4efaf55681453478631ebbf16bf1e51e58c
tunnckoCore <mameto_100@mail.bg>
2016-03-30 10:36:19 +0300

    cleanup

--- test.js
+++ test.js
@@ -123,29 +123,3 @@ test('should merge all contexts - from Ctor, from .compose and from .on', functi
     done()
   }, {e: 'f'}).compose('emit')('name')
 })
-
-// function foo () {
-//   console.log('foo', this)
-// }
-// function bar () {
-//   console.log('bar', this)
-// }
-
-// var app = new ComposeEmitter({context: {a: 'b'}})
-// app.once('foo', foo, {c: 'd'}).emit('foo').emit('foo') // => 'foo' once
-// app.on('foo', foo).on('foo', foo).emit('foo') // => 'foo' twice
-// app.on('foo', foo).off('foo', foo).emit('foo') // => nothing
-// app.on('foo', foo).on('bar', bar).off().emit() // => nothing
-// app.on('foo', foo).on('bar', bar).off('foo').emit() // => 'bar'
-// app.on('foo', foo).on('bar', bar).off('foo').emit('foo') // => nothing
-// app.on('foo', foo).on('bar', bar).off().emit('foo') // => returns this
-// app.on('foo', foo).emit('abc') // returns this
-
-  // .on('foo', function () {
-  //   console.log('foo1')
-  // })
-  // .on('foo', function () {
-  //   console.log('foo2')
-  // })
-  // .emit('foo')
-  // .emit('foo')
